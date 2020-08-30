faba0cdbfdfa8ea55838252c8a565d1d2ec84b0c
viznev <viznev@users.noreply.github.com>
2020-08-30 02:45:46 -0500

    setup engines, catchall, and dev express static

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); // req.body
 
+app.use(express.static("./client/build"));
+
 if (process.env.NODE_ENV === "production") {
     // serve static content
     app.use(express.static(path.join(__dirname,"client/build")));
@@ -85,6 +87,10 @@ app.delete("/todos/:id", async (req, res) => {
     } catch (err) {
         console.error(err.message);
     }
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 app.listen(PORT, () => {
