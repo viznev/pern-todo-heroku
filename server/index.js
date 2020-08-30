0245156c73a76fcccfa5bbb761997aa69be86058
viznev <viznev@users.noreply.github.com>
2020-08-29 23:16:55 -0500

    get a todo or all todos

--- server/index.js
+++ server/index.js
@@ -25,8 +25,28 @@ app.post("/todos", async (req, res) => {
 });
 
 // GET all  todos
+app.get("/todos", async (req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 // GET a todo
+app.get("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const todo = await pool.query(
+            "SELECT * FROM todo WHERE todo_id = $1",
+            [id]
+        );
+        res.json(todo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 // UPDATE a todo
 
