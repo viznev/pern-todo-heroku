25c791904e96a6e496d9120a37a58175bd05d166
viznev <viznev@users.noreply.github.com>
2020-08-29 23:09:46 -0500

    create a todo

--- server/index.js
+++ server/index.js
@@ -5,9 +5,32 @@ const pool = require("./db");
 
 // middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // req.body
 
+// ROUTES //
 
+// CREATE a todo
+app.post("/todos", async (req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query(
+            "INSERT INTO todo (description) VALUES($1) RETURNING *",
+            [description]
+        );
+
+        res.json(newTodo.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// GET all  todos
+
+// GET a todo
+
+// UPDATE a todo
+
+// DELETE a todo
 
 app.listen(5000, () => {
     console.log("Server started on port 5000.")
