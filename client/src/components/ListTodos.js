ccd2f27b678232df252c168f7061b77fd1246ccd
viznev <viznev@users.noreply.github.com>
2020-08-30 00:50:55 -0500

    built delete todo component

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -4,6 +4,19 @@ const ListTodos = () => {
 
     const [todos, setTodos] = useState([]);
 
+    // delete todo function
+    const deleteTodo = async (id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+            });
+
+            setTodos(todos.filter(todo => todo.todo_id !== id));
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
     const getTodos = async () => {
         try {
             const response = await fetch("http://localhost:5000/todos")
@@ -14,7 +27,7 @@ const ListTodos = () => {
             console.error(err.message);
         }
     }
-    
+
     useEffect(() => {
         getTodos();
     }, []);
@@ -37,10 +50,17 @@ const ListTodos = () => {
                 </tr>
                 */}
                 {todos.map(todo => (
-                    <tr>
+                    <tr key={todo.todo_id}>
                         <td>{todo.description}</td>
                         <td>Edit</td>
-                        <td>Delete</td>
+                        <td>
+                            <button
+                                className="btn btn-danger"
+                                onClick={() => deleteTodo(todo.todo_id)}
+                            >
+                                Delete
+                                </button>
+                        </td>
                     </tr>
                 ))}
             </tbody>
