dbd8b7545c4be4c8f849aaf57cf6c81dee0b5ef2
viznev <viznev@users.noreply.github.com>
2020-08-29 22:58:33 -0500

    setup DB connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
 
 app.listen(5000, () => {
     console.log("Server started on port 5000.")
